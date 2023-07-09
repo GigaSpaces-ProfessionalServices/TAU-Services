e24b14c1b830033a1c8f5eee54d5fa0de8c77c8c
yuvaldori <yuvaldori38@gmail.com>
2023-07-09 09:58:05 +0300

    Added //todo with instructions

--- blueprints/one-table-service/templates/src/main/java/com/gs/usecase/{{service.name}}JdbcTask.java
+++ blueprints/one-table-service/templates/src/main/java/com/gs/usecase/{{service.name}}JdbcTask.java
@@ -11,10 +11,14 @@ import org.slf4j.LoggerFactory;
 public class {{service.name}}JdbcTask extends GeneralTask<{{service.name}}Request, {{service.name}}Response> implements Task<ArrayList<{{service.name}}Response>> {
     private static final Logger logger = LoggerFactory.getLogger({{service.name}}Response.class);
 
+    //todo change here the number of query params and request params acording to what you have in the values.yaml file
+    // this query will be used when setting the 2 parameters
     static private final String USECASE_QUERY1 ="SELECT {{query.param1}},{{query.param2}},{{query.param3}},{{query.param4}} " +
             "FROM {{service.type}} " +
             "WHERE {{request.param1}} = '%s' AND {{request.param2}} = '%s'";
 
+    //todo change here the number of query params and request params acording to what you have in the values.yaml file.
+    // this query will be used when setting just the first parameter
     static private final String USECASE_QUERY2 ="SELECT {{query.param1}},{{query.param2}},{{query.param3}},{{query.param4}} " +
         "FROM {{service.type}} " +
         "WHERE {{request.param1}} = '%s'";
@@ -27,6 +31,7 @@ public class {{service.name}}JdbcTask extends GeneralTask<{{service.name}}Reques
     public ArrayList<{{service.name}}Response> execute() {
 
         String query = null;
+        //todo change here the number of query params and request params acording to what you have in the values.yaml file.
         if (request.get{{request.param2}}() != null){
             query=String.format(USECASE_QUERY1,request.get{{request.param1}}(),request.get{{request.param2}}());
         }else{
