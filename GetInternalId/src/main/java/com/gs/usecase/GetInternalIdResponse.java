3c1e10f8fddfe51b296e76ff904877e4c0a6a899
yuvaldori <yuvaldori38@gmail.com>
2023-07-13 15:14:26 +0300

    Convert K_PNIMI from E Notation to decimal

--- GetInternalId/src/main/java/com/gs/usecase/GetInternalIdResponse.java
+++ GetInternalId/src/main/java/com/gs/usecase/GetInternalIdResponse.java
@@ -1,6 +1,8 @@
 package com.gs.usecase;
 
 import com.gs.infra.service.ServiceResponse;
+
+import java.math.*;
 import java.sql.*;
 
 //todo change this properties and methods, so it will fit to the number of query params in the values.yaml file.
@@ -9,8 +11,9 @@ public class GetInternalIdResponse implements ServiceResponse {
     String K_PNIMI;
 
     public GetInternalIdResponse(ResultSet rs) throws SQLException{
+        BigDecimal bigDecimal = new BigDecimal(rs.getString("K_PNIMI"));
 
-        setK_PNIMI(rs.getString("K_PNIMI"));
+        setK_PNIMI(bigDecimal.toPlainString());
     }
 
     public void setK_PNIMI(String K_PNIMI) {
