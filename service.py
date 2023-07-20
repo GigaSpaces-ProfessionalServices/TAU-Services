1a692a551aa2f6b97e50a55e2fe1c4294bb5123a
Alon Segal <segalalon@gmail.com>
2023-07-20 14:21:51 +0300

    deploy pu: changed space name to bllservice

--- service.py
+++ service.py
@@ -178,7 +178,7 @@ def deploy_service():
             "consul.host": "localhost", 
             "minPort": 8115, 
             "maxPort": 8311,
-            "space": "bllspace"
+            "space": "bllservice"
         }
         }).replace("'",'"')
     deploy_pu_data = requests.post(base_url, data=payload, headers=h)
