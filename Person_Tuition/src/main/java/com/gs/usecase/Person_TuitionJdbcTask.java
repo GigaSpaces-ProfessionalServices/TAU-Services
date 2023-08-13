560fe73554d9815a0f1dced713cd2871364eeaa4
yuvaldori <yuvald@gigaspaces.com>
2023-08-13 15:59:01 +0300

    Person Tuition Service V2

--- Person_Tuition/src/main/java/com/gs/usecase/Person_TuitionJdbcTask.java
+++ Person_Tuition/src/main/java/com/gs/usecase/Person_TuitionJdbcTask.java
@@ -10,21 +10,21 @@ import java.util.*;
 
 
 public class Person_TuitionJdbcTask extends GeneralTask<Person_TuitionRequest, Person_TuitionResponse> implements Task<ArrayList<Person_TuitionResponse>> {
-    private static final Logger logger = LoggerFactory.getLogger(Person_TuitionResponse.class);
+    private static final Logger logger = LoggerFactory.getLogger(Person_TuitionJdbcTask.class);
 
     // STUD.SL_HESHBON sl - 2M rows
     // STUD.SL_TNUA st - 16.2M rows
     // STUD.TB_069_SCL_PEULA t069 - 197 rows
     // crd - see inside
-    static private final String USECASE_QUERY = "SELECT\n" +
-            "sl.k_pnimi,\n" +
-            "sl.k_sem,\n" +
-            "sl.yitra balance,\n" +
-            "crd.total_credits,\n" +
-            "dbt.total_debits FROM\n" +
+    static private final String USECASE_QUERY_MAIN = "SELECT\n" +
+            "sl.k_pnimi k_pnimi_q1,\n" +
+            "sl.k_sem k_sem_q1,\n" +
+            "sl.yitra balance_q1,\n" +
+            "crd.total_credits total_credits_q1,\n" +
+            "dbt.total_debits total_debits_q1\n" +
+            "FROM\n" +
             "STUD.SL_HESHBON sl\n" +
             "LEFT JOIN\n" +
-
             // start crd definition
             "(SELECT st.k_pnimi, k_sem, sum(st.schum) total_credits\n" +
             "FROM STUD.SL_TNUA st\n" +
@@ -37,7 +37,6 @@ public class Person_TuitionJdbcTask extends GeneralTask<Person_TuitionRequest, P
             "(sug='02' AND tat_sug='03'))\n" +
             "GROUP BY st.k_pnimi, k_sem) crd\n" +
             // end crd definition
-
             "ON crd.k_pnimi = sl.k_pnimi AND\n" +
             "sl.k_sem = crd.k_sem AND\n" +
             "crd.k_pnimi = %s\n" + // 2 - idno
@@ -57,6 +56,46 @@ public class Person_TuitionJdbcTask extends GeneralTask<Person_TuitionRequest, P
             "ORDER BY sl.k_sem\n" +
             "LIMIT %s"; // 7 - limit
 
+    static private final String USECASE_QUERY_CREDIT = "SELECT\n" +
+            "sl.k_pnimi k_pnimi_q2,\n" +
+            "sl.k_sem k_sem_q2,\n" +
+            "t069.teur teur_q2,\n" +
+            "t069.teur_eng teur_eng_q2,\n" +
+            "sl.schum schum_q2,\n" +
+            "sl.t_pticha t_pticha_q2\n" +
+            "FROM\n" +
+            "STUD.SL_TNUA sl\n" +
+            "JOIN STUD.TB_069_SCL_PEULA t069\n" +
+            "ON sl.kod_tnua = t069.k_code\n" +
+            "WHERE\n" +
+            "((sug='03' AND tat_sug IN ('01','02','04','99','09')) OR\n" +
+            "(kod_tnua IN ('0200','0210') AND schum<0 ) OR\n" +
+            "sug='01' OR (sug='02' AND tat_sug='03')) AND\n" +
+            "sl.k_pnimi = %s AND\n" + // 1 - idno
+            "sl.k_sem <= '%s'\n" + //  2 - from_year
+            "ORDER BY sl.t_pticha\n" +
+            "LIMIT %s"; // 3 - limit
+
+    static private final String USECASE_QUERY_DEBIT = "SELECT\n" +
+        "sl.k_pnimi k_pnimi_q3,\n" +
+        "sl.k_sem k_sem_q3,\n" +
+        "t069.teur teur_q3,\n" +
+        "t069.teur_eng teur_eng_q3,\n" +
+        "sl.schum schum_q3,\n" +
+        "sl.t_pticha t_pticha_q3\n" +
+        "FROM\n" +
+        "STUD.SL_TNUA sl\n" +
+        "JOIN STUD.TB_069_SCL_PEULA t069\n" +
+        "ON sl.kod_tnua = t069.k_code\n" +
+        "WHERE\n" +
+        "((sug='02' AND tat_sug IN ('01','02','04','05','06','07','08','10','13')) OR\n" +
+        "kod_tnua IN ('0140') OR\n" +
+        "sug='05' AND tat_sug='01') AND\n" +
+        "sl.k_pnimi = %s AND\n" + // 1 - idno
+        "sl.k_sem <= '%s'\n" + // 2 - from_year
+        "ORDER BY sl.t_pticha\n" +
+        "LIMIT %s"; // 3 - limit
+
 
     @Override
     public Integer routing() {
@@ -65,9 +104,13 @@ public class Person_TuitionJdbcTask extends GeneralTask<Person_TuitionRequest, P
 
     public ArrayList<Person_TuitionResponse> execute() {
 
-        String query = String.format(USECASE_QUERY, request.getK_PNIMI(), request.getK_PNIMI(),
+        String query_main = String.format(USECASE_QUERY_MAIN, request.getK_PNIMI(), request.getK_PNIMI(),
                 request.getFROM_YEAR(), request.getK_PNIMI(),
                 request.getK_PNIMI(), request.getFROM_YEAR(), request.getLimit());
+
+        String query_credit = String.format(USECASE_QUERY_CREDIT, request.getK_PNIMI(), request.getFROM_YEAR(),request.getLimit());
+
+        String query_debit = String.format(USECASE_QUERY_DEBIT, request.getK_PNIMI(), request.getFROM_YEAR(),request.getLimit());
 
         // A list to store the results
         ArrayList<Person_TuitionResponse> responseList = new ArrayList<>();
@@ -77,39 +120,49 @@ public class Person_TuitionJdbcTask extends GeneralTask<Person_TuitionRequest, P
 
         try (Connection con = DriverManager.getConnection(properties.getProperty("driverurl"), props)) {
 
-            Statement stmt = con.createStatement();
+            String[] queryArray = {query_main, query_credit, query_debit};
+            String[] queryTypeArray = {"main", "credit", "debit"};
+
             ResultSet resultSet = null;
+            int idex = 0;
+            Statement stmt = con.createStatement();
 
-            logger.info("query = " + query);
+            for (String query : queryArray) {
 
-            try {
-                resultSet = stmt.executeQuery(query);
+                logger.info("query = " + query);
 
-                ResultSetMetaData rsmd = resultSet.getMetaData();
-                int columnsNumber = rsmd.getColumnCount();
+                try {
+                    resultSet = stmt.executeQuery(query);
 
-                for (int k=1; k<= columnsNumber; k++) {
-                    if (k > 1) logger.info(",  ");
-                    logger.info(rsmd.getColumnName(k));
-                    logger.info("ClassName" + rsmd.getColumnClassName(k));
+                    ResultSetMetaData rsmd = resultSet.getMetaData();
+                    int columnsNumber = rsmd.getColumnCount();
+
+                    for (int k = 1; k <= columnsNumber; k++) {
+                        if (k > 1) logger.info(",  ");
+                        logger.info(rsmd.getColumnName(k));
+                        logger.info("ClassName" + rsmd.getColumnClassName(k));
+                    }
+
+                    logger.info("##### Got the resultSet ##### " + resultSet);
+
+                } catch (Throwable e) {
+                    logger.error("##### Got Exception while on stmt.executeQuery(query) ##### " + e);
                 }
 
-                logger.info("##### Got the resultSet ##### " + resultSet);
-            } catch (Throwable e) {
-                logger.error("##### Got Exception while on stmt.executeQuery(query) ##### " + e);
-            }
+                if (resultSet == null) {
+                    logger.info("resultSet is null");
+                    return responseList;
+                }
 
-            if (resultSet == null) {
-                logger.info("resultSet is null");
-                return responseList;
-            }
+                while (resultSet.next()) {
+                    logger.info("#####  idex " + idex + " #####");
+                    Person_TuitionResponse response = new Person_TuitionResponse(resultSet, queryTypeArray[idex]);
+                    responseList.add(response);
+                }
 
-            while (resultSet.next()) {
-                Person_TuitionResponse response = new Person_TuitionResponse(resultSet);
-                responseList.add(response);
+                idex++;
             }
-
-        } catch (SQLException sqlException) {
+        } catch(SQLException sqlException){
             logger.error("Cannot query database ", sqlException);
             return null;
         }
--- Person_Tuition/src/main/java/com/gs/usecase/Person_TuitionResponse.java
+++ Person_Tuition/src/main/java/com/gs/usecase/Person_TuitionResponse.java
@@ -1,7 +1,9 @@
 package com.gs.usecase;
 
 import com.gs.infra.service.ServiceResponse;
-import com.gigaspaces.document.SpaceDocument;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.math.BigDecimal;
 import java.sql.*;
 
@@ -10,24 +12,127 @@ import java.sql.*;
  */
 public class Person_TuitionResponse implements ServiceResponse {
 
-    public Person_TuitionResponse(ResultSet rs) throws SQLException {
+    private static final Logger logger = LoggerFactory.getLogger(Person_TuitionResponse.class);
 
-        setK_PNIMI(rs.getInt("k_pnimi"));
-        setK_SEM(rs.getString("k_sem"));
-        setBALANCE(rs.getBigDecimal("balance"));
-        setTOTAL_CREDITS(rs.getBigDecimal("total_credits"));
-        setTOTAL_DEBITS(rs.getBigDecimal("total_debits"));
+    public Person_TuitionResponse(ResultSet rs, String queryType) throws SQLException {
+
+        if (queryType.equals("main")) {
+            // Main
+            setK_PNIMI_Q1(rs.getInt("k_pnimi_q1"));
+            setK_SEM_Q1(rs.getString("k_sem_q1"));
+            setBALANCE_Q1(rs.getBigDecimal("balance_q1"));
+            setTOTAL_CREDITS_Q1(rs.getBigDecimal("total_credits_q1"));
+            setTOTAL_DEBITS_Q1(rs.getBigDecimal("total_debits_q1"));
+        } else if (queryType.equals("credit")) {
+            // credit
+            setK_PNIMI_Q2(rs.getInt("k_pnimi_q2"));
+            setK_SEM_Q2(rs.getString("k_sem_q2"));
+            setTEUR_Q2(rs.getString("teur_q2"));
+            setTEUR_ENG_Q2(rs.getString("teur_eng_q2"));
+            setSCHUM_Q2(rs.getBigDecimal("schum_q2"));
+            setT_PTICHA_Q2(rs.getString("t_pticha_q2"));
+        } else if (queryType.equals("debit")) {
+            // credit
+            setK_PNIMI_Q3(rs.getInt("k_pnimi_q3"));
+            setK_SEM_Q3(rs.getString("k_sem_q3"));
+            setTEUR_Q3(rs.getString("teur_q3"));
+            setTEUR_ENG_Q3(rs.getString("teur_eng_q3"));
+            setSCHUM_Q3(rs.getBigDecimal("schum_q3"));
+            setT_PTICHA_Q3(rs.getString("t_pticha_q3"));
+        }else {
+            logger.error("Wrong query type " + queryType);
+        }
     }
 
-    Integer K_PNIMI;
-    String K_SEM;
-    BigDecimal BALANCE;
-    BigDecimal TOTAL_CREDITS;
-    BigDecimal TOTAL_DEBITS;
+    // Main
+    Integer K_PNIMI_Q1;
+    String K_SEM_Q1;
+    BigDecimal BALANCE_Q1;
+    BigDecimal TOTAL_CREDITS_Q1;
+    BigDecimal TOTAL_DEBITS_Q1;
 
-    public void setK_PNIMI(Integer k_PNIMI) {K_PNIMI = k_PNIMI;}
-    public void setK_SEM(String k_SEM) {K_SEM = k_SEM;}
-    public void setBALANCE(BigDecimal BALANCE) {this.BALANCE = BALANCE;}
-    public void setTOTAL_CREDITS(BigDecimal TOTAL_CREDITS) {this.TOTAL_CREDITS = TOTAL_CREDITS;}
-    public void setTOTAL_DEBITS(BigDecimal TOTAL_DEBITS) {this.TOTAL_DEBITS = TOTAL_DEBITS;}
+    // Credit
+    Integer K_PNIMI_Q2;
+    String K_SEM_Q2;
+    String TEUR_Q2;
+    String TEUR_ENG_Q2;
+    String T_PTICHA_Q2;
+    BigDecimal SCHUM_Q2;
+
+
+    // Debit
+    Integer K_PNIMI_Q3;
+    String K_SEM_Q3;
+    String TEUR_Q3;
+    String TEUR_ENG_Q3;
+    String T_PTICHA_Q3;
+    BigDecimal SCHUM_Q3;
+
+    public void setK_PNIMI_Q1(Integer k_PNIMI_Q1) {
+        K_PNIMI_Q1 = k_PNIMI_Q1;
+    }
+
+    public void setK_SEM_Q1(String k_SEM_Q1) {
+        K_SEM_Q1 = k_SEM_Q1;
+    }
+
+    public void setBALANCE_Q1(BigDecimal BALANCE_Q1) {
+        this.BALANCE_Q1 = BALANCE_Q1.setScale(2, BigDecimal.ROUND_HALF_UP);
+}
+
+    public void setTOTAL_CREDITS_Q1(BigDecimal TOTAL_CREDITS_Q1) {
+        this.TOTAL_CREDITS_Q1 = TOTAL_CREDITS_Q1.setScale(2, BigDecimal.ROUND_HALF_UP);
+    }
+
+    public void setTOTAL_DEBITS_Q1(BigDecimal TOTAL_DEBITS_Q1) {
+        this.TOTAL_DEBITS_Q1 = TOTAL_DEBITS_Q1.setScale(2, BigDecimal.ROUND_HALF_UP);
+    }
+
+    public void setK_PNIMI_Q2(Integer k_PNIMI_Q2) {
+        K_PNIMI_Q2 = k_PNIMI_Q2;
+    }
+
+    public void setK_SEM_Q2(String k_SEM_Q2) {
+        K_SEM_Q2 = k_SEM_Q2;
+    }
+
+    public void setTEUR_Q2(String TEUR_Q2) {
+        this.TEUR_Q2 = TEUR_Q2;
+    }
+
+    public void setTEUR_ENG_Q2(String TEUR_ENG_Q2) {
+        this.TEUR_ENG_Q2 = TEUR_ENG_Q2;
+    }
+
+    public void setT_PTICHA_Q2(String t_PTICHA_Q2) {
+        T_PTICHA_Q2 = t_PTICHA_Q2;
+    }
+
+    public void setSCHUM_Q2(BigDecimal SCHUM_Q2) {
+        this.SCHUM_Q2 = SCHUM_Q2.setScale(2, BigDecimal.ROUND_HALF_UP);
+    }
+
+    public void setK_PNIMI_Q3(Integer k_PNIMI_Q3) {
+        K_PNIMI_Q3 = k_PNIMI_Q3;
+    }
+
+    public void setK_SEM_Q3(String k_SEM_Q3) {
+        K_SEM_Q3 = k_SEM_Q3;
+    }
+
+    public void setTEUR_Q3(String TEUR_Q3) {
+        this.TEUR_Q3 = TEUR_Q3;
+    }
+
+    public void setTEUR_ENG_Q3(String  TEUR_ENG_Q3) {
+        this.TEUR_ENG_Q3 = TEUR_ENG_Q3;
+    }
+
+    public void setT_PTICHA_Q3(String t_PTICHA_Q3) {
+        T_PTICHA_Q3 = t_PTICHA_Q3;
+    }
+
+    public void setSCHUM_Q3(BigDecimal SCHUM_Q3) {
+        this.SCHUM_Q3 = SCHUM_Q3.setScale(2, BigDecimal.ROUND_HALF_UP);
+    }
  }
