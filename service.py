3e815e9fd0e19d02dd0ede8356713e9a47d73f69
Alon Segal <segalalon@gmail.com>
2023-08-13 13:53:48 +0300

    added queries

--- service.py
+++ service.py
@@ -5,6 +5,7 @@ import requests
 import sys
 import os
 import re
+import socket
 import yaml
 import subprocess
 from time import sleep
@@ -12,7 +13,17 @@ from glob import glob
 
 
 def usage():
-    print(f"[INFO] usage: {__file__} [deploy | undeploy] environment service")
+    print("\nUsage:")
+    print(f"  {os.path.basename(__file__)} [command] environment [parameters: service_name | query_type [types | fields [TYPE_NAME]]]")
+    print("\nCommand:")
+    print(f"{'  deploy':<12}{'-':<5} deploy a service")
+    print(f"{'  undeploy':<12}{'-':<5} undeploy a service")
+    print(f"{'  test':<12}{'-':<5} test service health endpoint")
+    print(f"{'  query':<12}{'-':<5} query space data")
+    print("\nParameters:")
+    print(f"{'  service:':<12}{'-':<5} the service name required when command is not a query")
+    print(f"{'  type:':<12}{'-':<5} the type of query. on of (types | fields)")
+    print('\n')
 
 
 def pu_exist(service_name):
@@ -52,15 +63,45 @@ def get_service_resource(service_name):
             return item
 
 
+def get_space_types(space_id):
+    # return a list of types in space
+    h = {'Accept': 'application/json'}
+    base_url = f'http://{manager}:8090/v2/spaces/{space_id}/statistics/types'
+    types = [t for t in requests.get(base_url, headers=h, verify=False).json()]
+    return types
+
+
+def get_space_type_fields(space_id, _type):
+    # return a list of types in space
+    h = {'Accept': 'application/json'}
+    base_url = f'http://{manager}:8090/v2/spaces/{space_id}/objectsTypeInfo'
+    for obj in requests.get(base_url, headers=h, verify=False).json()['objectTypesMetadata']:
+        if obj['objectName'] == _type:
+            fields = [f['name'] for f in obj['schema']]
+            return fields
+
+
 def get_request_desc(request_id):
     data = requests.get(
-        f'http://{manager}:8090/v2/requests?status=All', 
-        headers={'Accept': 'application/json'}, 
+        f'http://{manager}:8090/v2/requests?status=All',
+        headers={'Accept': 'application/json'},
         verify=False).json()
     for item in data:
         if item['id'] == request_id:
             return item['description']
     return None
+
+
+def check_connection(_server, _port, _timeout=5):
+    check_port = 1
+    a_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    a_socket.settimeout(_timeout)
+    try:
+        check_port = a_socket.connect_ex((_server, _port))
+    except socket.error as socerr:
+        print(f"[ERROR] caught exception: {socerr}")
+    a_socket.settimeout(None)
+    return check_port == 0
 
 
 def is_rest_ok(manager):
@@ -103,7 +144,7 @@ def undeploy_service():
         print(f"[INFO] no containers found for service '{service}'")
     else:
         count, limit = 0, 3
-        while count < limit:        
+        while count < limit:
             if len(containers) > 0:
                 h = {'Accept': 'text/plain'}
                 base_url = f'http://{manager}:8090/v2/containers'
@@ -143,7 +184,7 @@ def deploy_service():
     # create containers
     h = {'Accept': 'text/plain', 'Content-Type': 'application/json'}
     base_url = f'http://{manager}:8090/v2/containers'
-    for host in SPACE_HOSTS:
+    for host in spaces:
         payload = str({"memory": "1g", "zone": service, "host": host}).replace("'",'"')
         create_cont_data = requests.post(base_url, data=payload, headers=h)
         print(f"[INFO] operation: {get_request_desc(create_cont_data.text)} (id: {create_cont_data.text})")
@@ -156,8 +197,8 @@ def deploy_service():
     base_url = f'http://{manager}:8090/v2/pus'
     resource = glob(f"{service}/target/*{service}*dependencies.jar")[0]
     upload = requests.put(
-        f"{base_url}/resources", 
-        files={'file': open(resource, 'rb')}, 
+        f"{base_url}/resources",
+        files={'file': open(resource, 'rb')},
         verify=False)
     print(f"[INFO] operation: Upload resource (id: {upload.text})")
     if int(upload.status_code) in range(200,300):
@@ -170,15 +211,15 @@ def deploy_service():
     base_url = f'http://{manager}:8090/v2/pus'
     h = {'Accept': 'text/plain', 'Content-Type': 'application/json'}
     payload = str({
-        "resource": resource_name, 
-        "topology":{"instances": 1}, 
-        "name":service, 
-        "sla": {"zones":[service]}, 
+        "resource": resource_name,
+        "topology":{"instances": 1},
+        "name":service,
+        "sla": {"zones":[service]},
         "contextProperties":{
-            "consul.host": "localhost", 
-            "minPort": 8115, 
+            "consul.host": "localhost",
+            "minPort": 8115,
             "maxPort": 8311,
-            "space": "dih-tau-space"
+            "space": f"{space_name}"
         }
         }).replace("'",'"')
     deploy_pu_data = requests.post(base_url, data=payload, headers=h)
@@ -208,50 +249,47 @@ def deploy_service():
 
 
 def test_service_health():
+
+    def get_ssl_file(_type):
+        if os.path.exists(ssl_root):
+            try:
+                the_file = glob(f"{ssl_root}/{_type}/*")[0]
+            except:
+                print(f"[ERROR] {_type} file could not be found in '{ssl_root}/{_type}/'.")
+        return the_file
+
     # get northbound applicative domain
     cmd = f"sudo su - root -c \"ssh {pivot} 'cat \$ENV_CONFIG/nb/applicative/nb.conf.template' | grep \"NB_DOMAIN\"\""
     nbconf = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE).stdout.decode()
     nb_domain = str(nbconf).rstrip('\n').replace('"','').split('=')[1]
     base_url = f'https://{nb_domain}:8443/{service}/v1/actuator/health'
-    
-    # supported ssl extensions
-    cert_types = ('*.cer', '*.crt')
-    
-    # check if the directory for selected environment exists
     ssl_root = f"ssl/{env_name}"
-    if os.path.exists(ssl_root):
-        try:
-            ca_file = [glob(f"{ssl_root}/ca/{t}") for t in cert_types][0]
-        except:
-            print("[ERROR] health check aborted. CA certificate could not be found.")
-            return
-        try:
-            cert_file = [glob(f"{ssl_root}/cert/{t}") for t in cert_types][0]
-        except:
-            print("[ERROR] health check aborted. certificate could not be found.")
-            return
-        try:
-            key_file = glob(f"{ssl_root}/key/*.key")[0]
-        except:
-            print("[ERROR] health check aborted. Key file could not be found.")
-            return
-    else:
-        print(f"[ERROR] health check aborted. Could not find certificates for environment '{env_name}'.")
-        return
-    
-    # execute health check
 
+    ca_file = get_ssl_file('ca')
+    cert_file = get_ssl_file('cert')
+    key_file = get_ssl_file('key')
+
+    for c in [ca_file, cert_file, key_file]:
+        if c is None:
+            print("[ERROR] missing required ssl files. aborting health check!")
+
+    # execute health check
     count, timeout = 1, 20
-    health_ok, ssl_ok = False, True
+    proxy = {
+        "http": "http://132.66.251.5:8080",
+        "https": "http://132.66.251.5:8080"
+    }
     print(f"[INFO] Checking service '{service}' health ...")
     while count < timeout:
         count += 1
         try:
-            response = requests.get(base_url, cert=(cert_file, key_file), verify=ca_file, timeout=5)
-            # response = requests.get(base_url, cert=(cert_file, key_file), verify=True, timeout=5)
+            if len(proxy) > 0:
+                response = requests.get(base_url, proxies=proxy, cert=(cert_file, key_file), verify=ca_file, timeout=5)
+            else:
+                response = requests.get(base_url, cert=(cert_file, key_file), verify=ca_file, timeout=5)
         except requests.exceptions.SSLError as serr:
             print(f"[ERROR]: request status - SSLError: \n{serr}")
-            ssl_ok = False
+            continue
         except requests.exceptions.Timeout:
             print("[WARNING]: request status - timeout reached.")
             sleep(1)
@@ -265,15 +303,22 @@ def test_service_health():
             continue
         else:
             if response.status_code == 200:
-                health_ok = True
-            else:
-                sleep(1)
-        if not ssl_ok:
-            break
-        if health_ok:    
-            text = response.text.replace('"','')
-            print(f"[INFO ]service '{service}' health check returned: {text}")
-            break
+                text = response.text.replace('"','')
+                print(f"[INFO ] service '{service}' health check returned: {text}")
+                break
+            sleep(1)
+
+
+def query_space(_query_type):
+    if _query_type == 'types':
+        #print(get_space_types(space_name))
+        rlist = '\n'.join(get_space_types(space_name))
+        #rlist = 'aaa\nbbb\nccc'
+        print(rlist)
+    if _query_type == 'fields':
+        rlist = '\n'.join(get_space_type_fields(space_name, field_type))
+        print(rlist)
+
 
 
 if __name__ == '__main__':
@@ -282,17 +327,30 @@ if __name__ == '__main__':
         print("[ERROR] missing required parameters")
         usage()
         exit(1)
-    else:
-        opt = sys.argv[1]
-        env_name = sys.argv[2]
+    opt = sys.argv[1]
+    env_name = sys.argv[2]
+    if opt != 'query':
         service = sys.argv[3]
-    
+    else:
+        qtype = sys.argv[3]
+
+    if qtype == 'fields':
+        if len(sys.argv) < 5:
+            print("[ERROR] missing TYPE_NAME parameter for fields query")
+            usage()
+            exit(1)
+        else:
+            field_type = sys.argv[4]
+
+    # the space id
+    space_name = "dih-tau-space"
+
     # parse environment
-    if env_name == "Development":
+    if env_name.casefold() == "development":
         env_key = 'TAUG'
-    elif env_name == "Stage":
+    elif env_name.casefold() == "stage":
         env_key = 'TAUS'
-    elif env_name == "Production":
+    elif env_name.casefold() == "production":
         env_key = 'TAUP'
     else:
         env_key = ''
@@ -310,28 +368,37 @@ if __name__ == '__main__':
         print("[ERROR] pivot is not set in /etc/environment for selected env_name")
         exit(1)
 
+    # check connection to pivot
+    if not check_connection(pivot, 22):
+        print(f"[ERROR] unable to connect via SSH to pivot '{pivot}'. operation aborted!")
+        exit(1)
+
     # load servers from host.yaml
     cmd = f"sudo su - root -c \"ssh {pivot} 'cat \$ENV_CONFIG/host.yaml'\""
     hosts = yaml.safe_load(subprocess.run(cmd, shell=True, stdout=subprocess.PIPE).stdout.decode())
-    # parse management hosts and set operational manager
-    MANAGER_HOSTS = [m for m in hosts['servers']['manager'].values()]
-    if len(MANAGER_HOSTS) == 0:
+
+    # get management hosts and set operational manager
+    managers = [m for m in hosts['servers']['manager'].values()]
+    if len(managers) == 0:
         print("[ERROR] DIH managers not defined. check host.yaml configuration on pivot host.")
         exit(1)
-    restful_managers = [m for m in MANAGER_HOSTS if is_rest_ok(m)]
-    manager = restful_managers[0]
+    restful_managers = [m for m in managers if is_rest_ok(m)]
+    manager = restful_managers[0]   # we use the 1st available manager
 
-    # parse space hosts
-    SPACE_HOSTS = [m for m in hosts['servers']['space'].values()]
-    if len(SPACE_HOSTS) == 0:
+    # get space hosts
+    spaces = [m for m in hosts['servers']['space'].values()]
+    if len(spaces) == 0:
         print("[ERROR] DIH spaces not defined. check host.yaml configuration on pivot host.")
         exit(1)
-    
+
+    # parse parameters
     if opt == 'deploy':
         deploy_service()
     elif opt == 'undeploy':
         undeploy_service()
     elif opt == 'test':
         test_service_health()
+    elif opt == 'query':
+        query_space(qtype)
     else:
         usage()
